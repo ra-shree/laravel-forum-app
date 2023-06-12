2ac35756c89c2efc8c17bd9e5dd3fdff94758f64
Aarya Shree Chapagain <caaryashree@gmail.com>
2023-06-12 16:50:54 +0545

    feat: added login and register pages

---
+++ resources/views/login.blade.php
@@ -0,0 +1,21 @@
+@extends('app')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6 col-sm-8">
+                <h2 class="mt-4">Login</h2>
+                <form>
+                    <div class="mb-3">
+                        <label for="loginEmail" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="loginEmail" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="loginPassword" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="loginPassword" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
