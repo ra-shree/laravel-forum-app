cc5fdfeddffc5ae37530a0e5e2dabf1bd7505039
Aarya Shree Chapagain <caaryashree@gmail.com>
2023-06-12 16:23:11 +0545

    feat: added users, posts, comments, replies table migrations

--- database/migrations/2023_06_12_094705_create_posts_table.php
+++ database/migrations/2023_06_12_094705_create_posts_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('title');
+            $table->string('excerpt');
+            $table->text('body');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('posts');
     }
 };
