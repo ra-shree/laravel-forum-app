5b864da33d2f7819c6b7b47f106c86ead223b224
Aarya Shree Chapagain <caaryashree@gmail.com>
2023-06-15 14:13:48 +0545

    feat:guests can also now comment on posts

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -10,16 +10,20 @@ class CommentController extends Controller
 {
     public function create()
     {
-        $user = auth()->user();
-
         $attributes = request()->validate([
             'post_id' => ['required', Rule::exists('posts', 'id')],
             'content' => ['required', 'max:300']
         ]);
 
-        $attributes['user_id'] = $user->id;
-        Comment::create($attributes);
+        if(auth()->check()) {
+            $user_id = auth()->user()->id;
+        } else {
+            $user_id = null;
+        }
 
+        $attributes['user_id'] = $user_id;
+
+        Comment::create($attributes);
         return back()->with('success', 'Comment Added');
     }
 }
